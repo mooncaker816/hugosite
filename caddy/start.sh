a3afcd28657f04ef59748e92771228e28ccc9b1e
Mingle <MingleGu@gmail.com>
2018-04-28 20:18:49 +0800

    update

--- caddy/start.sh
+++ caddy/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker run  -d \
+docker run -d --rm \
     -e "ACME_AGREE=true" \
-    -e "SECRET=123456" \
+    -e "SECRET=xxxxxx" \
     -e "CADDYPATH=/etc/caddycerts" \
     -v ~/.ssh/id_rsa:/home/.ssh/id_rsa \
     -v $(pwd)/Caddyfile:/etc/Caddyfile \
     -v $HOME/.caddy:/etc/caddycerts \
     -v /var/log/caddy:/var/log/caddy \
     -p 80:80 -p 443:443 \
-    mooncaker816/mycaddy
+    mooncaker816/caddy-hugo
